28dd96015c5b640b983597dc1827ae4172f0b4a8
MShajieShah <shajieshah@gmail.com>
2021-10-12 19:54:02 +0500

    Complete Calendar Functionality implemented

--- calendar.rb
+++ calendar.rb
@@ -1,30 +1,51 @@
 require "date"
 
-def days_in_month(year, month)
-  Date.new(year.to_i, month.to_i, -1).strftime("%d")
-end
-
-def weekday(year, month, date)
-  Date.new(year.to_i, month.to_i, date.to_i).strftime("%A")
-end
-
-def month_by_year(year, month)
-  a = days_in_month(year, month)
+def days_by_month(year, month)
+  a = Date.new(year, month, -1).day
+  j = Hash.new
   for i in 1..a
-    weekday(year, month, date)
+    j[i] = Date.new(year, month, i).strftime("%A")
   end
+  j
+end
+
+def month_by_year(year)
+  k = Hash.new
+  k[year] = Hash.new
+  for i in 1..12
+    k[year][(Date.new(year, i).strftime("%B")) + " #{year} "] = days_by_month(year, i)
+  end
+  k
 end
 
 puts "Enter Year"
-year = gets.chomp
-puts "Enter Month"
-month = gets.chomp
-puts "Enter Date"
-date = gets.chomp
-puts "-" * 20
-puts "Outputs"
-p days_in_month(year, month)
-p weekday(year, month, date)
+year = gets.chomp.to_i
+puts "------------------------"
+puts "| Calendar Of The Year |"
+puts "------------------------"
 
-p Date::ABBR_MONTHNAMES[1..-1]
-p month_by_year(year, month)
+if year.digits.count == 4
+  p month_by_year(year)
+elsif year.digits.count == 2
+  if year == 70
+    p "Re-confirm your Year 1970 or 2070"
+    year = gets.chomp.to_i
+    p month_by_year(year)
+  else
+    if year.to_i >= 71 && year.to_i <= 99
+      year = ("19" + "#{year}").to_i
+      p month_by_year(year)
+    else year >= 99 && year <= 69
+      year = ("20" + "#{year}").to_i
+      p month_by_year(year)     end
+  end
+else
+  p "Year is not correct"
+end
+p "Do you want to search a month? Press 'y' for yes or any key to quit!!!"
+c = gets.chomp
+if c.downcase == "y"
+  p "Enter a Month from 1-12"
+  month = gets.chomp.to_i
+  p days_by_month(year, month)
+end
